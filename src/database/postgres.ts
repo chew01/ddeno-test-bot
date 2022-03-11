e2dd0024d982aae66b337f35b6be19eb5d0aa3a3
Daniel Chew <53224241+chew01@users.noreply.github.com>
2022-03-11 20:43:46 +0800

    Initial commit

---
+++ src/database/postgres.ts
@@ -0,0 +1,19 @@
+import { DB_HOSTNAME, DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME, POOL_CONNECTIONS } from "../../configs.ts";
+import { Pool } from "/../../deps.ts";
+
+export const dbPool = new Pool({
+  database: DB_NAME,
+  hostname: DB_HOSTNAME,
+  user: DB_USERNAME,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+  tls: {
+    enabled: false,
+  },
+}, POOL_CONNECTIONS);
+
+const client = await dbPool.connect();
+await client.queryArray(
+  "CREATE TABLE IF NOT EXISTS commandVersions (guildId BIGINT NOT NULL PRIMARY KEY, version INTEGER NOT NULL)",
+);
+client.release();
