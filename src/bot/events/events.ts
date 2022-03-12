af2be6affce3479b64a1c8732d79b1fba454ae58
chew01 <chewjh19@gmail.com>
2022-03-13 00:40:13 +0800

    Backtracking on message commands

--- src/bot/events/events.ts
+++ src/bot/events/events.ts
@@ -1,7 +1,5 @@
 import { setInteractionCreateEvent } from "./handlers/interactionCreate.ts";
-import { setMessageCreateEvent } from "./handlers/messageCreate.ts";
 
 export function setupEventHandlers() {
-  setMessageCreateEvent();
   setInteractionCreateEvent();
 }
